8e00110fe58bc2f1739a61d53d661024fed07490
JuanTiseira <jtiseira7@gmail.com>
2024-03-04 15:54:33 -0300

    Added create asistencia function

--- asistencia/views.py
+++ asistencia/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import AsistenciaSerializer
 from rest_framework.authentication import TokenAuthentication
-from api.permissions import CustomDjangoModelPermissions 
+from api.permissions import CustomDjangoModelPermissions
 from rest_framework.permissions import IsAuthenticated
 from api.permissions import IsAdminOrReadOnly
 from rest_framework.response import Response
@@ -13,15 +13,20 @@ from django.core.serializers import serialize
 from rest_framework.decorators import action
 from rest_framework import viewsets, status
 
+from carrera.models import Carrera
+from alumno.models import Alumno
+from materia.models import Materia
+from estado_asistencia.models import EstadoAsistencia
+
 class AsistenciaViewSet(viewsets.ModelViewSet):
     queryset = Asistencia.objects.prefetch_related(
         Prefetch('asistenciaalumno_set', queryset=AsistenciaAlumno.objects.select_related('estado'), to_attr='asistencias_alumnos')
     )
-    
+
     serializer_class = AsistenciaSerializer
     # permission_classes = [IsAdminOrReadOnly]
     authentication_classes = [TokenAuthentication]
-    
+
     def get_queryset(self):
         queryset = super().get_queryset()
         fecha = self.request.query_params.get('fecha')
@@ -43,7 +48,7 @@ class AsistenciaViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(materia=materia)
         if alumnos is not None:
             queryset = queryset.filter(asistenciaalumno__alumno_id=alumnos)
-        
+
         print(alumnos,queryset)
         return queryset
 
@@ -51,7 +56,7 @@ class AsistenciaViewSet(viewsets.ModelViewSet):
         context = super().get_serializer_context()
         context.update({'request': self.request})
         return context
-    
+
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
 
@@ -65,7 +70,7 @@ class AsistenciaViewSet(viewsets.ModelViewSet):
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
-    
+
     @action(detail=False, methods=['GET'])
     def view_asistencia(self, request):
         print("holaasas")
@@ -78,64 +83,37 @@ class AsistenciaViewSet(viewsets.ModelViewSet):
                         status= status.HTTP_201_CREATED)
         else:
             return JsonResponse({'error': 'Método no permitido'}, status=405)
-        
 
-    # def create(self, request, *args, **kwargs):
-    #     # Extraer datos del usuario del cuerpo de la solicitud
-    #     if "rol" in request.data:
-    #         rol_id = request.data['rol']
-    #         rol = Rol.objects.get(pk=rol_id)
-    #         request.data['rol'] = rol
-        
-    #     try:
-    #         with transaction.atomic():
-    #             user = None
-    #             if request.data["username"] is not None and request.data["password"] is not None:
-    #                 if len(request.data["username"]) > 0 and len(request.data["password"]) > 0:
-    #                     # rol = request.data['rol']
-    #                     # validate_password(request.data["password"])
-    #                     user = User.objects.create_user(username=request.data["username"].upper(), password=request.data["password"])
-    #                     if rol.grupo is not None:
-    #                         user.groups.add(rol.grupo)
-    #                         user.save()
-        
-    #             usuario = Usuario.objects.create(
-    #                             rol=request.data['rol'], 
-    #                             nombre=request.data['nombre'].upper(),
-    #                             apellido=request.data['apellido'].upper(),
-    #                             telefono=request.data['telefono'])
-                
-               
-    #             if len(request.data['dni']) > 0:
-    #                 usuario.dni = request.data['dni']
-                    
-    #             if len(request.data['direccion']) > 0:
-    #                 usuario.direccion = request.data['direccion'].upper()
-                    
-    #             if len(request.data['fecha_nacimiento']) > 0:
-    #                 usuario.fecha_nacimiento = request.data['fecha_nacimiento']
-                    
-    #             if len(request.data['email']) > 0:
-    #                 usuario.email = request.data['email'].upper()
-                    
-    #             if user is not None:
-    #                 usuario.user = user
-                    
-                
-    #             usuario.changed_by = request.user
-    #             usuario.save()
-    #             usuario = UsuarioSerializer(usuario, context={'request': request}).data
-    #             return Response(data={'success': True, 'message': "Usuario creado.", "data" : usuario},
-    #                         status= status.HTTP_201_CREATED)
-                
-    #     except ValidationError as ex:
-    #         return Response(data={'success': False, 'message': "No se pudo crear el usuario, contraseña inválida", 'error': str(ex)},
-    #                         status= status.HTTP_400_BAD_REQUEST)
-    #     except Exception as ex:
-    #         if len(ex.args[0].split("DETAIL: "))>1:
-    #             error = ex.args[0].split("DETAIL: ")[1]
-    #         else:
-    #             error = ex    
-    #         return Response(data={'success': False, 'message': "No se pudo crear el usuario", 'error': str(error)},
-    #                         status= status.HTTP_400_BAD_REQUEST)
-    
+
+
+    def create(self, request, *args, **kwargs):
+        try:
+            # Obtener datos del JSON
+            fecha = request.data.get('fecha')
+            carrera = request.data.get('carrera')
+            materia = request.data.get('materia')
+            alumnos_estado_asistencia = request.data.get('alumnos_estado_asistencia')  # Lista de diccionarios con id del alumno y estado de asistencia
+            # print("llegeu", carrera.get("id"), materia.get("id"))
+            # Obtener carrera y materia
+            carrera = Carrera.objects.get(id=carrera.get("id"))
+            materia = Materia.objects.get(id=materia.get("id"))
+            print(carrera.id, materia.id)
+            # # Crear la asistencia
+            # asistencia = Asistencia.objects.create(fecha=fecha, carrera=carrera, materia=materia)
+
+            # # Recorrer la lista de alumnos y sus estados de asistencia
+            # for alumno_estado in alumnos_estado_asistencia:
+            #     alumno_id = alumno_estado.get('alumno_id')
+            #     estado_asistencia_id = alumno_estado.get('estado_asistencia_id')
+
+            #     # Obtener el alumno y el estado de asistencia
+            #     alumno = Alumno.objects.get(id=alumno_id)
+            #     estado_asistencia = EstadoAsistencia.objects.get(id=estado_asistencia_id)
+
+            #     # Crear la relación entre la asistencia y el alumno con su estado de asistencia
+            #     AsistenciaAlumno.objects.create(asistencia=asistencia, alumno=alumno, estado=estado_asistencia)
+
+            return Response(data={'success': True, 'message': "Asistencia registrada correctamente."}, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response(data={'success': False, 'message': "Error al registrar la asistencia.", 'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
